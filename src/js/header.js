2b95ee695a336b268dab042da0c21602c66dd6f9
Polina Igrushkina <polina.igrushkina@gmail.com>
2022-09-28 20:25:07 +0300

    Header mobile

--- src/js/header.js
+++ src/js/header.js
@@ -5,19 +5,29 @@ const refs = {
   body: document.querySelector('body'),
   favorite: document.querySelector('.favorite'),
   favoriteDiv: document.querySelector('.favorite-div'),
+  favoriteModal: document.querySelector('.favorite-modal'),
+  favoriteDivModal: document.querySelector('.favorite-div-modal'),
 };
 
 refs.openModalBtn.addEventListener("click", toggleModal);
 refs.closeModalBtn.addEventListener("click", toggleModal);
-refs.favorite.addEventListener('click', onFavorireClick);
+refs.favorite.addEventListener('click', onFavoriteClick);
+refs.favoriteModal.addEventListener('click', onFavoriteModalClick);
 
 function toggleModal() {
   refs.modal.classList.toggle("is-hidden");
   refs.body.classList.toggle("no-scroll");
 };
 
-function onFavorireClick(event) {
+function onFavoriteClick(event) {
   event.preventDefault();
 
   refs.favoriteDiv.classList.toggle('none');
 }
+
+function onFavoriteModalClick(event) {
+  event.preventDefault();
+
+  refs.favoriteDivModal.classList.toggle('none');
+}
+
