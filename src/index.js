837d065109ef09138da7baa2e8b19e8fbbf92ceb
DanilStan <stanislavov.danik@gmail.com>
2022-09-28 20:05:52 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,50 +1,32 @@
-// import { getElement } from './js/ingredient_modal';
-import axios from 'axios';
+import { getElement, add, createIngredientMarkup } from './js/ingredient_modal';
+const divs = document.querySelector('.modal2');
 
-async function getElement() {
-  const response = await axios.get(
-    'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
-  );
-  const data = await response.drinks;
+getElement('vodka').then(data => {
+  const markup = createIngredientMarkup(data.ingredients);
+  add(markup);
+});
 
-  return data;
-}
+// __________________________________________________________________________
+const onClickAdd = document.querySelector('.modal2__btn__add');
+console.log(onClickAdd);
 
-getElement();
+onClickAdd.addEventListener('click', onClickBtnAdd);
 
-crateImageMarkUp(getElement());
+// Код для localStorage
+// const save = (key, value) => {
+//   try {
+//     const serializedState = JSON.stringify(value);
+//     localStorage.setItem(key, serializedState);
+//   } catch (error) {
+//     console.error('Set state error: ', error.message);
+//   }
+// };
 
-function crateImageMarkUp(response) {
-  console.log(response);
-  return response
-    .map(({ strDrinkThumb, strDrink }) => {
-      return `<div class="main__container">
-        <img class="main__img" src="${strDrinkThumb}" alt="negroni" />
-        <div class="main__text-container">
-          <h3 class="main__title-second">${strDrink}</h3>
-          <div class="main__button-center">
-            <button class="button-orang button">Learn more</button>
-            <button class="button-transparent button">
-              Add to<span
-                ><img
-                  src="../images/heart-min.png"
-                  alt="heart"
-                  width="20px"
-                  height="18px"
-                  class="main__button-img"
-              /></span>
-              <span
-                ><img
-                  src="../images/full-heart.png"
-                  alt="heart"
-                  width="20px"
-                  height="18px"
-                  class="main__button-imgfull"
-              /></span>
-            </button>
-          </div>
-        </div>
-      </div>`;
-    })
-    .join('');
-}
+// const load = key => {
+//   try {
+//     const serializedState = localStorage.getItem(key);
+//     return serializedState === null ? undefined : JSON.parse(serializedState);
+//   } catch (error) {
+//     console.error('Get state error: ', error.message);
+//   }
+// };
