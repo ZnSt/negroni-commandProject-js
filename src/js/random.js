d2ce47b059f8e96e1f51c3596fe776b9dfbf7c11
Valeriia tymchenko <lera.tymchenko@ukr.net>
2022-09-29 15:17:30 +0300

    update random.file

--- src/js/random.js
+++ src/js/random.js
@@ -3,44 +3,93 @@ import fetchRandomCocktail from './fetch';
 import addMurkup from './add-murk-up';
 
 // const divRef = document.querySelector('.main__flex');
-
+const divRef = document.querySelector('.main__flex');
 let responseDrink = '';
+let allPromises = [];
+
+const windowScreen = window.screen.width;
+
+if (windowScreen < 768) {
+  getRandomData(3);
+}
+if (windowScreen >= 768 && windowScreen < 1280) {
+  getRandomData(6);
+}
+if (windowScreen >= 1280) {
+  getRandomData(12);
+}
 
 //запись данных в пустой массив
-function getRandomCoctails(size, promise) {
-  let primises = [];
-
+function getRandomCoctails(size) {
+  const promises = [];
   for (let i = 0; i < size; i++) {
-    primises.push(fetchRandomCocktail());
+    promises.push(fetchRandomCocktail());
   }
-  return primises;
+  return promises;
 }
 
 // создание и добавление разметки
-function returnAllCard() {
-  Promise.all(primises)
-    .then(data => {
-      const cardMurkUp = crateImageMarkUp(data);
-      return cardMurkUp;
-    })
-    .then(cardMurkUp => addMurkup(cardMurkUp))
+function returnAllCard(primises) {
+  return Promise.all(primises)
+    .then(data => data)
     .catch(error => console.log(error));
 }
-// const screenWidth = window.screen.width;
-
-function start() {
-  const promise = [];
-  getRandomCoctails(9, promise);
+// aунция для фильтрации
+function filterData(data = []) {
+  return data.filter(
+    ({ idDrink }, index, array) =>
+      array.findIndex(obj => obj.idDrink === idDrink) === index
+  );
 }
 
-function filter(elem, i, arr) {
-  const filterCard = [];
+let screenWidth = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    if (elem[i] === i) {
-      filterCard.push(this[i]);
-    }
+window.addEventListener(`resize`, checkSize);
+
+function checkSize(e) {
+  screenWidth = e.currentTarget.innerWidth;
+
+  console.log(screenWidth);
+  if (screenWidth < 768) {
+    clearContainer();
+    start(3);
   }
+  if (screenWidth >= 768 && screenWidth < 1280) {
+    clearContainer();
+    start(6);
+  }
+  if (screenWidth >= 1280) {
+    clearContainer();
+    start(12);
+  }
+}
+console.log(screenWidth);
 
-  return filterCard;
+function clearContainer() {
+  divRef.innerHTML = '';
+}
+
+// фильтрация промисей
+async function getRandomData(size) {
+  const promises = await getRandomCoctails(size);
+  const data = await returnAllCard(promises);
+  const fData = filterData(data);
+
+  allPromises = [...fData];
+  start(size);
+  //   fData.forEach(item => allPromises.push(item));
+  //   if (fData.length < size) {
+  //     const resultSize = size - fData.length;
+  //     start(resultSize, allPromises);
+  //   }
+  //   return allPromises.slice(0, size);
+  // }
+}
+
+async function start(number) {
+  const renderArray = [...allPromises];
+  renderArray.splice(number, allPromises.length);
+  //   console.log('allPromises :>> ', allPromises);
+  const create = await crateImageMarkUp(renderArray);
+  addMurkup(create);
 }
