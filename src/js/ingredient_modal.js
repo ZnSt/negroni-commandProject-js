5e4e1c4d9fadf239bd8e04f203c4fff4d29930f5
DanilStan <stanislavov.danik@gmail.com>
2022-09-29 13:39:30 +0300

    commit

--- src/js/ingredient_modal.js
+++ src/js/ingredient_modal.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 const divs = document.querySelector('.modal2');
 
-export function getElement(name) {
-  return fetch(
-    `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${name}`
-  ).then(response => {
-    return response.json();
-  });
-  //   return response.data;
+export async function getElement(name) {
+  try {
+    const response = await axios.get(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?i=${name}`
+    );
+    return response.data;
+  } catch (error) {
+    console.log('error', error);
+  }
 }
 
 // console.log(divs);
@@ -18,12 +20,12 @@ export function createIngredientMarkup(response = []) {
       return `<div class="modalw2">
             <div class="modalw2__p modalw2__border">
                 <h2 class="modalw2__title">${idIngredient}</h2>
-            <button class="modalw2__btn"><svg width="32px" height="32px">
+                <button class="modalw2__btn"><svg width="32px" height="32px">
                 <use
                   href=".//images/svg/symbol-defs.svg#icon-close"
                   class="modalw2__link"
                 ></use>
-              </svg></button>
+                </svg></button>
             <h3 class="modalw2__title-second">Liqueur</h3></div>
             <div class="modalw2__p">
                 <p class="modalw2__text"><span class="modalw2__span">Campari </span>is an Italian alcoholic liqueur, considered an apéritif (20.5%, 21%, 24%, 25%, or 28.5% ABV, depending on the country in which it is sold), obtained from the infusion of herbs and fruit (including chinotto and cascarilla) in alcohol and water. It is a bitters, characterised by its dark red colour.</p>
