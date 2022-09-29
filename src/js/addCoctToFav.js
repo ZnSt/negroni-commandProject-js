db61aca81a10d47b5550c76ea52a2d1261124d65
AndreiKrai <okraichenko.a@gmail.com>
2022-09-29 18:10:44 +0300

    problem with HTML

--- src/js/addCoctToFav.js
+++ src/js/addCoctToFav.js
@@ -29,55 +29,52 @@ function remFromLocalStCoctails(coctailID){
     }
     localStorage.removeItem('FAV_COCTAILS');
     localStorage.setItem('FAV_COCTAILS',JSON.stringify(favCoctArray))
+  }}
+
+
+
+
+  // --------------------------------------
+  function crateImageMarkUp(response) {
+    return response
+      .map(({ strDrinkThumb, strDrink, idDrink }) => {
+        return `<div  class="main__container">
+          <img class="main__img" src="${strDrinkThumb}" alt="negroni" />
+          <div class="main__text-container">
+            <h3 class="main__title-second">${strDrink}</h3>
+            <div class="main__button-center">
+              <button class="button-orange button">Learn more</button>
+              <button data-id="${idDrink}" class="button-transparent button">
+                Add to
+  <span><svg
+  class="main__button-img"
+    width="21"
+    height="19"
+    viewBox="0 0 21 19"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  ><path
+      d="M10.5 19L8.9775 17.6332C3.57 12.7978 0 9.60872 0 5.69482C0 2.50572 2.541 0 5.775 0C7.602 0 9.3555 0.838692 10.5 2.16403C11.6445 0.838692 13.398 0 15.225 0C18.459 0 21 2.50572 21 5.69482C21 9.60872 17.43 12.7978 12.0225 17.6436L10.5 19Z"
+      fill="#FD5103"
+    />
+    <path
+      d="M10.5 17L9.2675 15.921C4.89 12.1035 2 9.58583 2 6.49591C2 3.9782 4.057 2 6.675 2C8.154 2 9.5735 2.66213 10.5 3.70845C11.4265 2.66213 12.846 2 14.325 2C16.943 2 19 3.9782 19 6.49591C19 9.58583 16.11 12.1035 11.7325 15.9292L10.5 17Z"
+      fill="#FCFCFC"
+    />
+  </svg></span>
+  <span><svg class="main__button-imgfull" width="21" height="19" viewBox="0 0 21 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M10.5 19L8.9775 17.6332C3.57 12.7978 0 9.60872 0 5.69482C0 2.50572 2.541 0 5.775 0C7.602 0 9.3555 0.838692 10.5 2.16403C11.6445 0.838692 13.398 0 15.225 0C18.459 0 21 2.50572 21 5.69482C21 9.60872 17.43 12.7978 12.0225 17.6436L10.5 19Z" fill="#FD5103"/>
+  </svg></span>
+              </button>
+            </div>
+          </div>
+        </div>
+        `;
+      })
+      .join('');
   }
 
-
-// const API_KEY = '30131532-b39fadf9a6636e24080a2757a';
-// const BASE_URL = 'https://pixabay.com/api/?key=' + API_KEY + '&q=';
-// const options = '&image_type=photo&orientation=horizontal&safesearch=true&';
-// const perPage = 40;
-// import { page } from '../index';
-// function crateImageMarkUp({ strDrinkThumb, strDrink,idDrink}) {
-//   console.log(strDrinkThumb)
-//   console.log(strDrink)
-
-//   console.log( `<div class="main__container">
-//         <img class="main__img" src="${strDrinkThumb}" alt="negroni" />
-//         <div class="main__text-container">
-//           <h3 class="main__title-second">${strDrink}</h3>
-//           <div class="main__button-center">
-//             <button class="button-orang button" data-id="${idDrink}">Learn more</button>
-//             <button class="button-transparent button">
-//               Add to<span
-//                 ><img
-//                   src="../images/heart-min.png"
-//                   alt="heart"
-//                   width="20px"
-//                   height="18px"
-//                   class="main__button-img"
-//               /></span>
-//               <span
-//                 ><img
-//                   src="../images/full-heart.png"
-//                   alt="heart"
-//                   width="20px"
-//                   height="18px"
-//                   class="main__button-imgfull"
-//               /></span>
-//             </button>
-//           </div>
-//         </div>
-//       </div>
-//       `);
-// }
-// export async function getCoctail(input) {
-//   try {
-//     const response = await axios.get(
-//       'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
-//     );
-//     console.log(response.data.drinks[0])
-//     // crateImageMarkUp(response.data.drinks[0]) 
-//   } catch (error) {
-//     throw new Error(error);
-//   }
-// }
+  const divRef = document.querySelector('.main__flex');
+export default function addMurkup(arr = []) {
+  divRef.insertAdjacentHTML('beforeend', arr);
+}
