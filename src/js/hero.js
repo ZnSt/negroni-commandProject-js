b1642d203cd69a74bebfbd1fc86417c7652fea3a
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-09-30 00:25:12 +0300

    add logic witch press on letter

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,11 +1,28 @@
-const ulContainer = document.querySelector('.hero-list__mobi');
-console.log(ulContainer);
+import * as API from '../servises/cocktails-api';
+import crateImageMarkUp from './murk-up';
+import errorPicture from './error-markup';
+
+const ulContainer = document.querySelector('.hero-list');
+const renderContainer = document.querySelector('.main__flex');
 
 ulContainer.addEventListener('click', onClickBtn);
 
-function onClickBtn(event) {
-  console.log(event.target);
+async function onClickBtn(event) {
+  const value = event.target.dataset.value.toLowerCase();
   if (event.target.nodeName !== 'LI') {
     return;
+  }
+
+  try {
+    const responseData = await API.fetchGetData(value);
+    console.log(responseData);
+    const markup = crateImageMarkUp(responseData.drinks);
+
+    renderContainer.innerHTML = '';
+    renderContainer.insertAdjacentHTML('beforeend', markup);
+  } catch (error) {
+    renderContainer.innerHTML = '';
+    const errorPic = errorPicture();
+    renderContainer.insertAdjacentHTML('beforeend', errorPic);
   }
 }
