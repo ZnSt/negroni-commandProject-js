0b13416b6b4f2fad2bf4099f96be3c6fa36bc449
Brudja <n0669378587@gmail.com>
2022-09-29 13:25:50 +0300

    Исправляет комментарии

--- src/js/header.js
+++ src/js/header.js
@@ -1,29 +1,29 @@
-// const refs = {
-//   openModalBtn: document.querySelector("[data-header-modal-open]"),
-//   closeModalBtn: document.querySelector("[data-header-modal-close]"),
-//   modal: document.querySelector("[data-header-modal]"),
-//   body: document.querySelector('body'),
-//   favorite: document.querySelector('.favorite'),
-//   favoriteDiv: document.querySelector('.favorite-div'),
-//   favoriteModal: document.querySelector('.favorite-modal'),
-//   favoriteDivModal: document.querySelector('.favorite-div-modal'),
-// };
+const refs = {
+  openModalBtn: document.querySelector("[data-header-modal-open]"),
+  closeModalBtn: document.querySelector("[data-header-modal-close]"),
+  modal: document.querySelector("[data-header-modal]"),
+  body: document.querySelector('body'),
+  favorite: document.querySelector('.favorite'),
+  favoriteDiv: document.querySelector('.favorite-div'),
+  favoriteModal: document.querySelector('.favorite-modal'),
+  favoriteDivModal: document.querySelector('.favorite-div-modal'),
+};
 
-// refs.openModalBtn.addEventListener("click", toggleModal);
-// refs.closeModalBtn.addEventListener("click", toggleModal);
-// refs.favorite.addEventListener('click', onFavoriteClick);
-// refs.favoriteModal.addEventListener('click', onFavoriteModalClick);
+refs.openModalBtn.addEventListener("click", toggleModal);
+refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.favorite.addEventListener('click', onFavoriteClick);
+refs.favoriteModal.addEventListener('click', onFavoriteModalClick);
 
-// function toggleModal() {
-//   refs.modal.classList.toggle("is-hidden");
-//   refs.body.classList.toggle("no-scroll");
-// };
+function toggleModal() {
+  refs.modal.classList.toggle("is-hidden");
+  refs.body.classList.toggle("no-scroll");
+};
 
-// function onFavoriteClick(event) {
-//   event.preventDefault();
+function onFavoriteClick(event) {
+  event.preventDefault();
 
-//   refs.favoriteDiv.classList.toggle('none');
-// }
+  refs.favoriteDiv.classList.toggle('none');
+}
 
 // function onFavoriteModalClick(event) {
 //   event.preventDefault();
