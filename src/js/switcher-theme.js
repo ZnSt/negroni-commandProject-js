234ccf8f3074f1394c61673b8c1f85c58b5189c9
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-10-02 20:16:51 +0300

    add logic with switcher

--- src/js/switcher-theme.js
+++ src/js/switcher-theme.js
@@ -1,3 +1,20 @@
-const themeSwitchDeskRef = document.querySelector('[data-switcher-desktop]');
-const themeSwitchMobRef = document.querySelector('[data-switcher-mobil]');
+const inputSwEl = document.querySelector('.dark-button');
 
+inputSwEl.addEventListener('click', event => {
+  const currentTheme = document.documentElement.dataset.theme;
+  if (currentTheme === 'light') {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+});
+
+const KEY = 'theme';
+
+function getItem() {
+  localStorage.getItem(KEY);
+}
+
+function setItem(value) {
+  localStorage.setItem(KEY, value);
+}
